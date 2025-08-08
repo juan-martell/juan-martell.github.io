4711c204a01170c20be327f4f92430c3f1d4d00f
juan-martell <juanbak129@gmail.com>
2025-08-08 19:18:42 +0000

    agrego CNAME

--- script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-python /root/meli/meli.py
-cd /root/meli/juan-martell.github.io/
-git pull
-git add /root/meli/juan-martell.github.io/
-git commit -m "actualizo top.json"
-git push origin main
